c369fe5e8666e3ed10278bdcdad411450996bffc
Azat Ilyazov <ilyazovazat@gmail.com>
2022-09-06 21:08:29 +0300

    implemented 2-5 review points

--- deeppavlov_dreamtools/tests/test_distweather.py
+++ deeppavlov_dreamtools/tests/test_distweather.py
@@ -5,13 +5,13 @@ import pytest
 
 @pytest.fixture(scope="module")
 def dream_weather_dist_dir():
-    dream_path_dist = Path(Path().absolute().parents[2]) / "dream" / "assistant_dists" / "dream_weather"
+    dream_path_dist = Path(Path().absolute().parents[1]) / "dream" / "assistant_dists" / "dream_weather"
     yield dream_path_dist
 
 
 @pytest.fixture(scope="module")
 def files_in_dream_weather_dist_dir():
-    dream_path_dist = Path(Path().absolute().parents[2]) / "dream" / "assistant_dists" / "dream_weather"
+    dream_path_dist = Path().absolute().parents[1] / "dream" / "assistant_dists" / "dream_weather"
     files = [file.name for file in dream_path_dist.iterdir()]
     yield files
 
@@ -29,12 +29,13 @@ def test_dist_file_in_dream_directory(file: str, dream_weather_dist_dir, files_i
 def test_dream_weather_dist_corresponds_ground_truth_files(file: str, dream_weather_dist_dir) -> None:
     """
     Test if built files are equal to ground-truth files that are based in `ground_truth_path`.
-    :param file: name of file
-    :param dream_weather_dist_dir: path to root Dream directory
-    :return:
+
+    Args:
+        file (str): name of file
+        dream_weather_dist_dir (Path): path to root Dream directory
     """
     config_path = dream_weather_dist_dir / file
-    ground_truth_path = Path(Path().absolute().parent) / "static" / "dream_weather_dist_configs" / file
+    ground_truth_path = Path().absolute() / "static" / "dream_weather_dist_configs" / file
     with open(ground_truth_path) as ground_truth:
         with open(config_path) as dist_file:
             assert ground_truth.readlines() == dist_file.readlines(), f"built-file {file} differs from" \
