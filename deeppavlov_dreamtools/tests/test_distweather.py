4ea875ee3e1fdef2da706ee42c8a0f91a144ed89
Azat Ilyazov <ilyazovazat@gmail.com>
2022-09-06 18:05:01 +0300

    add test checking weather_dist files

---
+++ deeppavlov_dreamtools/tests/test_distweather.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+
+import pytest
+
+
+@pytest.fixture(scope="module")
+def dream_weather_dist_dir():
+    dream_path_dist = Path(Path().absolute().parents[2]) / "dream" / "assistant_dists" / "dream_weather"
+    yield dream_path_dist
+
+
+@pytest.fixture(scope="module")
+def files_in_dream_weather_dist_dir():
+    dream_path_dist = Path(Path().absolute().parents[2]) / "dream" / "assistant_dists" / "dream_weather"
+    files = [file.name for file in dream_path_dist.iterdir()]
+    yield files
+
+
+def test_if_dream_weather_dist_exists(dream_weather_dist_dir) -> None:
+    assert dream_weather_dist_dir.exists(), f"There is no directory on path: {dream_weather_dist_dir}"
+
+
+@pytest.mark.parametrize("file", ["dev.yml", "docker-compose.override.yml", "pipeline_conf.json", "proxy.yml"])
+def test_dist_file_in_dream_directory(file: str, dream_weather_dist_dir, files_in_dream_weather_dist_dir) -> None:
+    assert file in files_in_dream_weather_dist_dir, f"The file {file} is not in directory"
+
+
+@pytest.mark.parametrize("file", ["dev.yml", "docker-compose.override.yml", "pipeline_conf.json", "proxy.yml"])
+def test_dream_weather_dist_corresponds_ground_truth_files(file: str, dream_weather_dist_dir) -> None:
+    """
+    Test if built files are equal to ground-truth files that are based in `ground_truth_path`.
+    :param file: name of file
+    :param dream_weather_dist_dir: path to root Dream directory
+    :return:
+    """
+    config_path = dream_weather_dist_dir / file
+    ground_truth_path = Path(Path().absolute().parent) / "static" / "dream_weather_dist_configs" / file
+    with open(ground_truth_path) as ground_truth:
+        with open(config_path) as dist_file:
+            assert ground_truth.readlines() == dist_file.readlines(), f"built-file {file} differs from" \
+                                                                      f" the ground_truth file"
