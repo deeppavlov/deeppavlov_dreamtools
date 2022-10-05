6a7eea507249c75d4d92e02a025cd75ad65e9c7e
Azat Ilyazov <ilyazovazat@gmail.com>
2022-10-05 14:27:30 +0600

    add list_dists test and format black

--- deeppavlov_dreamtools/tests/test_distweather.py
+++ deeppavlov_dreamtools/tests/test_distweather.py
@@ -1,83 +1,46 @@
-import pytest
-
 from pathlib import Path
-from typing import Union
 
+import pytest
 from deeppavlov_dreamtools import DreamDist
+from deeppavlov_dreamtools.distconfigs import list_dists
+from deeppavlov_dreamtools.tests.fixtures import (
+    create_weather_dist,
+    dream_weather_dist_dir,
+    files_in_dream_weather_dist_dir,
+    list_of_dream_dist,
+)
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture
 def dream_root_dir(pytestconfig):
     yield Path(pytestconfig.getoption("dream_root"))
 
 
-@pytest.fixture(scope="module")
-def create_weather_dist(dream_root_dir):
-    template_name = "dream"
-    name = "dream_weather"
-    services = ['convers-evaluator-annotator', 'spacy-nounphrases', 'convers-evaluator-selector',
-                'dff-intent-responder-skill', 'intent-catcher', 'ner', 'entity-detection', 'dff-weather-skill',
-                'dialogpt']
-    pipeline_conf = True
-    compose_override = True
-    compose_dev = True
-    compose_proxy = True
-    compose_local = False
-    overwrite = True
-
-    dream_dist = DreamDist.from_name(
-        template_name,
-        dream_root_dir,
-        pipeline_conf,
-        compose_override,
-        compose_dev,
-        compose_proxy,
-        compose_local,
-    )
-    new_dist = dream_dist.create_dist(
-        name,
-        dream_root_dir,
-        services,
-        pipeline_conf,
-        compose_override,
-        compose_dev,
-        compose_proxy,
-        compose_local,
-    )
-    paths = new_dist.save(overwrite)
-
-    yield paths
-
-
-@pytest.fixture(scope="module")
-def dream_weather_dist_dir(create_weather_dist, dream_root_dir):
-    """
-    Path to directory where should be built files
-    """
-    dream_path_dist = dream_root_dir / "assistant_dists" / "dream_weather"
-    yield dream_path_dist
-
-
-@pytest.fixture(scope="module")
-def files_in_dream_weather_dist_dir(dream_weather_dist_dir):
-    """
-    List of files in the dream_weather distribution directory
-    """
-    files = [file.name for file in dream_weather_dist_dir.iterdir()]
-    yield files
-
-
 def test_if_dream_weather_dist_exists(dream_weather_dist_dir) -> None:
-    assert dream_weather_dist_dir.exists(), f"There is no directory at path: {dream_weather_dist_dir}"
+    assert (
+        dream_weather_dist_dir.exists()
+    ), f"There is no directory at path: {dream_weather_dist_dir}"
 
 
-@pytest.mark.parametrize("file", ["dev.yml", "docker-compose.override.yml", "pipeline_conf.json", "proxy.yml"])
-def test_dist_file_in_dream_directory(file: str, dream_weather_dist_dir, files_in_dream_weather_dist_dir) -> None:
-    assert file in files_in_dream_weather_dist_dir, f"The file {file} is not in the right directory"
+@pytest.mark.parametrize(
+    "file",
+    ["dev.yml", "docker-compose.override.yml", "pipeline_conf.json", "proxy.yml"],
+)
+def test_dist_file_in_dream_directory(
+    file: str, dream_weather_dist_dir, files_in_dream_weather_dist_dir
+) -> None:
+    assert (
+        file in files_in_dream_weather_dist_dir
+    ), f"The file {file} is not in the right directory"
 
 
-@pytest.mark.parametrize("file", ["dev.yml", "docker-compose.override.yml", "pipeline_conf.json", "proxy.yml"])
-def test_dream_weather_dist_corresponds_ground_truth_files(file: str, dream_weather_dist_dir) -> None:
+@pytest.mark.parametrize(
+    "file",
+    ["dev.yml", "docker-compose.override.yml", "pipeline_conf.json", "proxy.yml"],
+)
+def test_dream_weather_dist_corresponds_ground_truth_files(
+    file: str, dream_weather_dist_dir
+) -> None:
     """
     Test if built files are equal to ground-truth files that are based in `ground_truth_path`.
     If files aren't equal test shows which lines differ and prints those two different lines.
@@ -87,7 +50,9 @@ def test_dream_weather_dist_corresponds_ground_truth_files(file: str, dream_weat
         dream_weather_dist_dir (Path): path to root Dream directory
     """
     config_path = dream_weather_dist_dir / file
-    ground_truth_path = Path(__file__).parents[1] / "static" / "dream_weather_dist_configs" / file
+    ground_truth_path = (
+        Path(__file__).parents[1] / "static" / "dream_weather_dist_configs" / file
+    )
 
     with open(ground_truth_path) as ground_truth_file:
         with open(config_path) as dist_file:
@@ -102,4 +67,6 @@ def test_dream_weather_dist_corresponds_ground_truth_files(file: str, dream_weat
                     print(f"{ground_truth_text[i]} != {dist_file_text[i]}")
                     differ_lines.append(i + 1)
 
-            assert (not differ_lines), f"built-file {file} differs from the ground_truth_file at lines: {differ_lines} "
+            assert (
+                not differ_lines
+            ), f"built-file {file} differs from the ground_truth_file at lines: {differ_lines} "
---
+++ deeppavlov_dreamtools/tests/test_functional.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+import pytest
+from deeppavlov_dreamtools.tests.fixtures import (
+    DreamDist,
+    dream_assistant_dist_dir,
+    dream_root_dir,
+    list_of_dream_dist,
+)
+
+
+def test_list_dream_dist_is_not_empty(list_of_dream_dist):
+    assert len(list_of_dream_dist) > 0
+
+
+def test_items_in_dream_dist_is_DreamDist_objects(list_of_dream_dist):
+    for dreamdist_item in list_of_dream_dist:
+        assert isinstance(
+            dreamdist_item, DreamDist
+        ), f"{type(dreamdist_item)=} and {type(DreamDist)=}"
+
+
+def test_length_of_dreamdist_list_matches_the_number_of_directories(
+    list_of_dream_dist, dream_assistant_dist_dir
+):
+    count_of_dist_in_folder = 0
+    for item in dream_assistant_dist_dir.iterdir():
+        if item.is_dir():
+            count_of_dist_in_folder += 1
+    assert count_of_dist_in_folder == len(list_of_dream_dist)
