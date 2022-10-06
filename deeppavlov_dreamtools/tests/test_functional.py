fa1705ad6ad458a9f016b065af82e91ac1af9f66
Azat Ilyazov <ilyazovazat@gmail.com>
2022-10-06 12:56:08 +0600

    black format

--- deeppavlov_dreamtools/tests/test_functional.py
+++ deeppavlov_dreamtools/tests/test_functional.py
@@ -15,14 +15,10 @@ def test_list_dream_dist_is_not_empty(list_of_dream_dist):
 
 def test_items_in_dream_dist_is_DreamDist_objects(list_of_dream_dist):
     for dreamdist_item in list_of_dream_dist:
-        assert isinstance(
-            dreamdist_item, DreamDist
-        ), f"{type(dreamdist_item)=} and {type(DreamDist)=}"
+        assert isinstance(dreamdist_item, DreamDist), f"{type(dreamdist_item)=} and {type(DreamDist)=}"
 
 
-def test_length_of_dreamdist_list_matches_the_number_of_directories(
-    list_of_dream_dist, dream_assistant_dist_dir
-):
+def test_length_of_dreamdist_list_matches_the_number_of_directories(list_of_dream_dist, dream_assistant_dist_dir):
     count_of_dist_in_folder = 0
     for item in dream_assistant_dist_dir.iterdir():
         if item.is_dir():
