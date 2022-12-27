a370bbba2483b8f1884db261950a746be93ef6d9
ilyazat <ilyazovazat@gmail.com>
2022-12-27 09:56:24 +0300

    add tests checking ports

--- deeppavlov_dreamtools/tests/test_functional.py
+++ deeppavlov_dreamtools/tests/test_functional.py
@@ -24,3 +24,16 @@ def test_length_of_dreamdist_list_matches_the_number_of_directories(list_of_drea
         if item.is_dir():
             count_of_dist_in_folder += 1
     assert count_of_dist_in_folder == len(list_of_dream_dist)
+
+
+def test_discover_ports(dream_root_dir):
+    dream_dist = DreamDist.from_name("dream", dream_root_dir)
+
+    assert dream_dist.compose_override.discover_port("personality-catcher") == 8010
+
+    pipeline_service = dream_dist.pipeline_conf.config.services.skills["dff_program_y_skill"]
+    assert dream_dist.pipeline_conf.discover_port(pipeline_service) == 8008
+
+    assert dream_dist.compose_dev.discover_port("sentseg") == 8011
+
+    assert dream_dist.compose_proxy.discover_port("sentrewrite") == 8017
