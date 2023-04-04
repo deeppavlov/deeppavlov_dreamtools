aa182c20c6e8d514c1a9fab2d1424fa6212d2ff6
ilyazat <ilyazovazat@gmail.com>
2023-04-04 12:58:52 +0300

    fix: length test

--- deeppavlov_dreamtools/tests/test_functional.py
+++ deeppavlov_dreamtools/tests/test_functional.py
@@ -6,6 +6,7 @@ from deeppavlov_dreamtools.tests.fixtures import (
     dream_assistant_dists_dir,
     dream_root_dir,
     list_of_dream_dist,
+    list_dists
 )
 
 
@@ -18,12 +19,13 @@ def test_items_in_dream_dist_is_DreamDist_objects(list_of_dream_dist):
         assert isinstance(dreamdist_item, AssistantDist), f"{type(dreamdist_item)=} and {type(AssistantDist)=}"
 
 
-def test_length_of_dreamdist_list_matches_the_number_of_directories(list_of_dream_dist, dream_assistant_dists_dir):
+def test_length_of_dreamdist_list_matches_the_number_of_directories(dream_root_dir, dream_assistant_dists_dir):
     count_of_dist_in_folder = 0
-    for item in dream_assistant_dists_dir.iterdir():
-        if item.is_dir():
+    dists = list_dists(dream_root_dir)
+    for item in dists:
+        if item.dist_path.is_dir():
             count_of_dist_in_folder += 1
-    assert count_of_dist_in_folder == len(list_of_dream_dist)
+    assert count_of_dist_in_folder == len(dists)
 
 
 def test_discover_ports(dream_root_dir):
