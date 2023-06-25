0a431649e92da0fafc8351a9e7de18d777119b8c
mtalimanchuk <mtalimanchuk@gmail.com>
2023-06-26 03:12:00 +0400

    fix cli new, clone dist

--- deeppavlov_dreamtools/cmd.py
+++ deeppavlov_dreamtools/cmd.py
@@ -1,9 +1,13 @@
 from functools import wraps
 from pathlib import Path
+from typing import List
 
 import click
 
 from deeppavlov_dreamtools import commands, AssistantDist
+from deeppavlov_dreamtools.distconfigs.components import DreamComponent
+from deeppavlov_dreamtools.distconfigs.generics import PipelineConfMetadata
+from deeppavlov_dreamtools.distconfigs.pipeline import Pipeline
 
 
 class ContextObject:
@@ -73,91 +77,180 @@ def new(ctx: click.Context):
     """Create new distribution or skill"""
 
 
-@new.command("dff")
-@click.argument("name")
-@click.option("-d", "--dist", required=True, help="Dream distribution name")
-@click.option("-p", "--port", required=True, help="DFF skill port")
-@click.option(
-    "--all",
-    "all_configs",
-    is_flag=True,
-    default=False,
-    help="Add definition to all docker-compose configs (defaults to False). Overrides all other --compose-* flags",
-)
-@click.option(
-    "--compose-override/--no-compose-override",
-    default=False,
-    help="Add definition to docker-compose.override.yml (defaults to False)",
-)
-@click.option(
-    "--compose-dev/--no-compose-dev",
-    default=False,
-    help="Add definition to dev.yml config (defaults to False)",
-)
-@click.option(
-    "--compose-proxy/--no-compose-proxy",
-    default=False,
-    help="Add definition to proxy.yml config (defaults to False)",
-)
-@click.option(
-    "--compose-local/--no-compose-local",
-    default=False,
-    help="Add definition to local.yml config (defaults to False)",
-)
-@click.pass_context
-@must_be_inside_dream
-def new_dff(
-    ctx: click.Context,
-    name: str,
-    dist: str,
-    port: int,
-    all_configs: bool,
-    compose_override: bool,
-    compose_dev: bool,
-    compose_proxy: bool,
-    compose_local: bool,
-):
-    """Creates new dff skill in ./skills"""
-    if all_configs:
-        compose_override = compose_dev = compose_proxy = compose_local = True
-
-    new_dff_path = commands.new.dff(
-        name,
-        ctx.obj.dream_root,
-        dist,
-        port,
-        compose_override,
-        compose_dev,
-        compose_proxy,
-        compose_local,
-    )
-    click.echo(
-        f"Created new dff skill at {new_dff_path}.\n"
-        f"Don't forget to define your state formatter '{name}_formatter' "
-        f"in 'dream/state_formatters/dp_formatters.py'"
-    )
+# @new.command("dff")
+# @click.argument("name")
+# @click.option("-d", "--dist", required=True, help="Dream distribution name")
+# @click.option("-p", "--port", required=True, help="DFF skill port")
+# @click.option(
+#     "--all",
+#     "all_configs",
+#     is_flag=True,
+#     default=False,
+#     help="Add definition to all docker-compose configs (defaults to False). Overrides all other --compose-* flags",
+# )
+# @click.option(
+#     "--compose-override/--no-compose-override",
+#     default=False,
+#     help="Add definition to docker-compose.override.yml (defaults to False)",
+# )
+# @click.option(
+#     "--compose-dev/--no-compose-dev",
+#     default=False,
+#     help="Add definition to dev.yml config (defaults to False)",
+# )
+# @click.option(
+#     "--compose-proxy/--no-compose-proxy",
+#     default=False,
+#     help="Add definition to proxy.yml config (defaults to False)",
+# )
+# @click.option(
+#     "--compose-local/--no-compose-local",
+#     default=False,
+#     help="Add definition to local.yml config (defaults to False)",
+# )
+# @click.pass_context
+# @must_be_inside_dream
+# def new_dff(
+#     ctx: click.Context,
+#     name: str,
+#     dist: str,
+#     port: int,
+#     all_configs: bool,
+#     compose_override: bool,
+#     compose_dev: bool,
+#     compose_proxy: bool,
+#     compose_local: bool,
+# ):
+#     """Creates new dff skill in ./skills"""
+#     if all_configs:
+#         compose_override = compose_dev = compose_proxy = compose_local = True
+#
+#     new_dff_path = commands.new.dff(
+#         name,
+#         ctx.obj.dream_root,
+#         dist,
+#         port,
+#         compose_override,
+#         compose_dev,
+#         compose_proxy,
+#         compose_local,
+#     )
+#     click.echo(
+#         f"Created new dff skill at {new_dff_path}.\n"
+#         f"Don't forget to define your state formatter '{name}_formatter' "
+#         f"in 'dream/state_formatters/dp_formatters.py'"
+#     )
 
 
 @new.command("dist")
 @click.argument("name")
 @click.option("--display-name", help="Human-readable distribution name")
+@click.option("--author", help="Author email")
 @click.option("--description", help="Distribution description")
 @click.option(
     "--overwrite/--no-overwrite",
     default=False,
     help="Overwrite distribution directory if it exists",
 )
+@click.option(
+    "--last-chance-service",
+    multiple=False,
+    help="Last chance service to include in the distribution",
+)
+@click.option(
+    "--timeout-service",
+    multiple=False,
+    help="Timeout service to include in the distribution",
+)
+@click.option(
+    "--annotators",
+    multiple=True,
+    help="Annotators to include in the distribution (can be specified multiple times)",
+)
+@click.option(
+    "--response-annotators",
+    multiple=True,
+    help="Response Annotators to include in the distribution (can be specified multiple times)",
+)
+@click.option(
+    "--response-annotator-selectors",
+    multiple=False,
+    help="Response Annotator Selector to include in the distribution",
+)
+@click.option(
+    "--candidate-annotators",
+    multiple=True,
+    help="Candidate annotators to include in the distribution (can be specified multiple times)",
+)
+@click.option(
+    "--skill-selectors",
+    multiple=True,
+    help="Skill Selectors to include in the distribution (can be specified multiple times)",
+)
+@click.option(
+    "--skills",
+    multiple=True,
+    help="Skills to include in the distribution (can be specified multiple times)",
+)
+@click.option(
+    "--response-selectors",
+    multiple=True,
+    help="Response Selectors to include in the distribution (can be specified multiple times)",
+)
 @click.pass_context
 @must_be_inside_dream
-def new_dist(ctx: click.Context, name: str, display_name: str, description: str, overwrite: bool):
+def new_dist(
+    ctx: click.Context,
+    name: str,
+    display_name: str,
+    author: str,
+    description: str,
+    overwrite: bool,
+    last_chance_service: str,
+    timeout_service: str,
+    annotators: List[str],
+    response_annotators: List[str],
+    response_annotator_selectors: str,
+    candidate_annotators: List[str],
+    skill_selectors: List[str],
+    skills: List[str],
+    response_selectors: List[str],
+):
     """Creates new distribution in ./assistant_dists"""
 
-    try:
-        dist = AssistantDist.from_name("dream_persona_prompted", ctx.obj.dream_root)
-        cloned_dist = dist.clone(name, display_name, description)
-        cloned_dist.save(overwrite)
+    def _load_components_from_user_values(components: List[str]):
+        component_dict = {}
+        for c in components:
+            component = DreamComponent.from_file(c, ctx.obj.dream_root)
+            component_dict[component.component.name] = component
+        return component_dict
 
-        click.echo(f"Created new Dream distribution {cloned_dist.name}")
+    try:
+        dist = AssistantDist(
+            dist_path=ctx.obj.dream_root / "assistant_dists" / name,
+            name=name,
+            dream_root=ctx.obj.dream_root,
+            pipeline=Pipeline(
+                config=None,
+                metadata=PipelineConfMetadata(
+                    display_name=display_name,
+                    author=author,
+                    description=description,
+                ),
+                last_chance_service=DreamComponent.from_file(last_chance_service, ctx.obj.dream_root),
+                timeout_service=DreamComponent.from_file(timeout_service, ctx.obj.dream_root),
+                response_annotator_selectors=DreamComponent.from_file(response_annotator_selectors, ctx.obj.dream_root),
+                annotators=_load_components_from_user_values(annotators),
+                response_annotators=_load_components_from_user_values(response_annotators),
+                candidate_annotators=_load_components_from_user_values(candidate_annotators),
+                skill_selectors=_load_components_from_user_values(skill_selectors),
+                skills=_load_components_from_user_values(skills),
+                response_selectors=_load_components_from_user_values(response_selectors),
+            ),
+        )
+        dist.save(overwrite=overwrite, generate_configs=True)
+
+        click.echo(f"Created new Dream distribution {dist.name}")
     except FileExistsError:
         raise click.ClickException(
             f"{name} distribution already exists! "
@@ -211,8 +304,9 @@ def clone(ctx: click.Context):
 
 @clone.command("dist")
 @click.argument("name")
-@click.option("--template", help="Name of the original distribution")
+@click.option("--template", help="Name of the original distribution", required=True)
 @click.option("--display-name", help="Human-readable distribution name")
+@click.option("--author", help="Author email")
 @click.option("--description", help="Distribution description")
 @click.option(
     "--overwrite/--no-overwrite",
@@ -221,12 +315,14 @@ def clone(ctx: click.Context):
 )
 @click.pass_context
 @must_be_inside_dream
-def clone_dist(ctx: click.Context, name: str, template: str, display_name: str, description: str, overwrite: bool):
+def clone_dist(
+    ctx: click.Context, name: str, template: str, display_name: str, author: str, description: str, overwrite: bool
+):
     """Clones distribution from a template in ./assistant_dists"""
 
     try:
         dist = AssistantDist.from_name(template, ctx.obj.dream_root)
-        cloned_dist = dist.clone(name, display_name, description)
+        cloned_dist = dist.clone(name, display_name, author, description, existing_prompted_skills=[])
         cloned_dist.save(overwrite)
 
         click.echo(f"Created new Dream distribution {cloned_dist.name}")
